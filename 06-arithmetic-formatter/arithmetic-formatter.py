b220394aeb4fe24184f66b33fa32076ef473cbe1
DaveJV <djvozniak@gmail.com>
2025-08-04 15:11:26 -0600

    directory correction

--- 06-arithmetic-formatter/arithmetic-formatter.py
+++ 06-arithmetic-formatter/arithmetic-formatter.py
@@ -1,5 +1,8 @@
 def arithmetic_arranger(problems, show_answers=False):
+    if len(problems) > 5:
+        return "Error: Too many problems."
 
+        
     return problems
 
 print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
